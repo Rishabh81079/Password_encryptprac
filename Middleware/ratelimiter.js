43d3ad0b6ed6200156a1ea83b9d4c65ef9ea50a7
Rishabh <Rishabh94121@gmail.com>
2026-04-21 20:22:57 +0530

    ratelimiter sliding window done

--- Middleware/ratelimiter.js
+++ Middleware/ratelimiter.js
@@ -5,17 +5,22 @@ const ratelimiter = async (req,res,next) =>{
 
     try{
     const ip = req.ip
-    const count = await client.incr(ip)
+    const key = `rate:${ip}`
+    const current_time = Date.now()
 
-    if(count == 1)
-    await client.expire("3600")
+    await client.zRemRangeByScore(key,0, current_time)
 
-    if(count>10)
-        throw new Error("request limit exceeded")
-    
+    const count = await client.zCard(key)
+
+    if (count >= 5) {
+  return res.status(429).send("Too many requests ❌")
+}
+
+    await client.zAdd(key ,current_time, current_time )
+
+    await client.expire(key, 60)
+
     }
-
-
     catch(err){
         res.send(err.message)
     }
